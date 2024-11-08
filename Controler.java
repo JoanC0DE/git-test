ebb419c21799393c1082330bf484530070da47bf
JoanC0DE <cwierz.joanna@gmail.com>
2024-11-08 22:22:41 +0100

    Files modified Main.java and Controler.java

--- Controler.java
+++ Controler.java
@@ -1,1 +1,6 @@
 //komentarz 2
+public class Controler {
+    public static void main(String[] args) {
+        System.out.println("Hello, controler!");
+    }
+}
--- Main.java
+++ Main.java
@@ -1,1 +1,6 @@
 //komentarz 1
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, main!");
+    }
+}
